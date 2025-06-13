3b4bb4a5bc48b930095a52cf7485a6ca8f77fbc5
Nishrit Baral <nishritbaral@gmail.com>
2025-06-13 21:32:04 +0545

    made some changes in nav bar

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -30,7 +30,7 @@ export default function RootLayout({
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         <Navbar />
-        <hr />
+
         <ReactQueryClientProvider>{children}</ReactQueryClientProvider>
       </body>
     </html>
--- frontend/components/Navbar.tsx
+++ frontend/components/Navbar.tsx
@@ -16,11 +16,13 @@ export default function Navbar() {
   const pathName = usePathname();
   return (
     <>
-      <nav className="fixed top-0 left-0 w-full bg-transparent backdrop-blur z-50">
+      <nav className="top-0 left-0 w-full bg-transparent backdrop-blur z-50">
         <div className="max-w-7xl mx-auto flex items-center justify-between px-5 h-[10vh]">
-          <div className="relative w-12 h-12 rounded-full overflow-hidden">
-            <Image src="/Logo.png" alt="Header logo" fill objectFit="cover" />
-          </div>
+          <Link href="/">
+            <div className="relative w-12 h-12 rounded-full overflow-hidden">
+              <Image src="/Logo.png" alt="Header logo" fill objectFit="cover" />
+            </div>
+          </Link>
 
           <ul className="flex items-center gap-6 text-lg font-roboto">
             {links.map((item, index) => (
